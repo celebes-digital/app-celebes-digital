a3d0a9ddfbd6341c6d878dd227096f6b65eb3ea1
MeongGanas <farreluken@gmail.com>
2024-11-13 11:08:04 +0800

    menambah filament

--- routes/web.php
+++ routes/web.php
@@ -28,5 +28,3 @@ Route::middleware('auth')->prefix('/admin')->group(function () {
     // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-require __DIR__ . '/auth.php';
