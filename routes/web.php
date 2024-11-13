daebf52e0b9c0eef634e56ceee7ecc2071373352
MeongGanas <farreluken@gmail.com>
2024-11-13 09:49:31 +0800

    memperbaiki struktur folder

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,32 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome');
+    return Inertia::render('GuestUser/Welcome');
 });
 
 Route::get('/case', function () {
-    return Inertia::render('CaseStudy');
+    return Inertia::render('GuestUser/CaseStudy');
 });
 
 Route::get('/case/{case}/detail', function () {
-    return Inertia::render('CaseDetail');
+    return Inertia::render('GuestUser/CaseDetail');
 });
 
 Route::get('/contact', function () {
-    return Inertia::render('Contact');
+    return Inertia::render('GuestUser/Contact');
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware('auth')->prefix('/admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
 
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__ . '/auth.php';
